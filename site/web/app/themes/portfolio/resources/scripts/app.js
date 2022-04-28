83ca5471777735a719d369775af85ef3529a6ad7
Joseph Gross <joseph@ignitro.com>
2022-04-28 11:04:39 -0400

    JS Changes

--- site/web/app/themes/portfolio/resources/scripts/app.js
+++ site/web/app/themes/portfolio/resources/scripts/app.js
@@ -6,13 +6,12 @@ import Isotope from 'isotope-layout';
 let $ = jQuery;
 
 $(window).on("load", function() {
-  
+
   $(".preloader").addClass("active");
   setTimeout(function () {
     $(".preloader").addClass("done");
   }, 1000);
 });
-
 
 /**
  * app.main
@@ -24,61 +23,8 @@ const main = async (err) => {
   }
 
   isotopeMasonry();
-
   animatedProgressBar();
-	windowHieght();
-  
-  function animatedProgressBar () {
-		$(".progress").each(function() {
-			var skillValue = $(this).find(".skill-lavel").attr("data-skill-value");
-			$(this).find(".bar").animate({
-				width: skillValue,
-			}, 1500);
-
-			$(this).find(".skill-lavel").text(skillValue);
-		});
-	}
-
-	function windowHieght(){
-		if ( $(window).height() <=768 ) {
-			$(".pt-table").addClass("desktop-768");
-		} else {
-			$(".pt-table").removeClass("desktop-768");
-		}
-	}
-
-  /*----------------------------------------
-		Isotope Masonry
-	------------------------------------------*/
-	function isotopeMasonry() {
-    var gutterSelector = document.querySelector('.isotope-gutter');
-		var iso;
-    if(gutterSelector) {
-      iso = new Isotope( gutterSelector, {
-        itemSelector: '[class^="col-"]',
-        percentPosition: true,
-      });
-    }
-
-    var noGutterSelector = document.querySelector('.isotope-no-gutter');
-    if(noGutterSelector) {
-      new Isotope( noGutterSelector, {
-        itemSelector: '[class^="col-"]',
-        percentPosition: true,
-        masonry: {
-          columnWidth: 1,
-        },
-      });
-    }
-
-		$(".filter a").on("click", function() {
-			$(".filter a").removeClass("active");
-			$(this).addClass("active");
-      iso.arrange({ filter: $(this).attr("data-filter") });
-			
-			return false;
-		});
-	}
+  windowHieght();
 };
 
 /**
@@ -88,3 +34,56 @@ const main = async (err) => {
  */
 domReady(main);
 import.meta.webpackHot?.accept(main);
+
+
+function animatedProgressBar () {
+  $(".progress").each(function() {
+    var skillValue = $(this).find(".skill-lavel").attr("data-skill-value");
+    $(this).find(".bar").animate({
+      width: skillValue,
+    }, 1500);
+
+    $(this).find(".skill-lavel").text(skillValue);
+  });
+}
+
+function windowHieght(){
+  if ( $(window).height() <=768 ) {
+    $(".pt-table").addClass("desktop-768");
+  } else {
+    $(".pt-table").removeClass("desktop-768");
+  }
+}
+
+/*----------------------------------------
+  Isotope Masonry
+------------------------------------------*/
+function isotopeMasonry() {
+  var gutterSelector = document.querySelector('.isotope-gutter');
+  var iso;
+  if(gutterSelector) {
+    iso = new Isotope( gutterSelector, {
+      itemSelector: '[class^="col-"]',
+      percentPosition: true,
+    });
+  }
+
+  var noGutterSelector = document.querySelector('.isotope-no-gutter');
+  if(noGutterSelector) {
+    new Isotope( noGutterSelector, {
+      itemSelector: '[class^="col-"]',
+      percentPosition: true,
+      masonry: {
+        columnWidth: 1,
+      },
+    });
+  }
+
+  $(".filter a").on("click", function() {
+    $(".filter a").removeClass("active");
+    $(this).addClass("active");
+    iso.arrange({ filter: $(this).attr("data-filter") });
+    
+    return false;
+  });
+}
