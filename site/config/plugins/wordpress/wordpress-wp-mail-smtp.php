400776eb167e59c21a5fd1bd05e5724b16f4d9f0
Joseph Gross <joseph@ignitro.com>
2022-04-28 12:38:30 -0400

    added php extensions

--- site/config/plugins/wordpress/wordpress-wp-mail-smtp.php
+++ site/config/plugins/wordpress/wordpress-wp-mail-smtp.php
@@ -4,22 +4,20 @@
  * @url: https://wordpress.org/plugins/wp-mail-smtp/
  */
 
-// define('WPMS_ON', true);
 
-/* if (!empty(getenv('SENDGRID_USERNAME')) && !empty(getenv('SENDGRID_PASSWORD'))) {
-    // Auth method ('credentials')
-    
-    define('WPMS_MAILER', 'smtp');
-    
-    define( 'WPMS_SMTP_HOST', 'smtp.sendgrid.net' ); // The SMTP mail host.
-    define( 'WPMS_SMTP_PORT', 587 ); // The SMTP server port number.
-    define( 'WPMS_SSL', 'tls' ); // Possible values '', 'ssl', 'tls' - note TLS is not STARTTLS.
-    define( 'WPMS_SMTP_AUTH', true ); // True turns it on, false turns it off.
-    define( 'WPMS_SMTP_USER', getenv('SENDGRID_USERNAME') ); // SMTP authentication username, only used if WPMS_SMTP_AUTH is true.
-    define( 'WPMS_SMTP_PASS', getenv('SENDGRID_PASSWORD') ); // SMTP authentication password, only used if WPMS_SMTP_AUTH is true.
-    define( 'WPMS_SMTP_AUTOTLS', true ); // True turns it on, false turns it off.
-} else if (!empty(getenv('SENDGRID_API_KEY'))) {
+
+ if (!empty(getenv('SENDGRID_API_KEY'))) {
+
+    define( 'WPMS_ON', true );
+
+    define( 'WPMS_LICENSE_KEY', env('WPMS_LICENSE_KEY') ); 
+    define( 'WPMS_MAIL_FROM', env('MAIL_FROM') );
+    define( 'WPMS_MAIL_FROM_FORCE', true );
+    define( 'WPMS_MAIL_FROM_NAME', getenv('MAIL_FROM_NAME') );
+    define( 'WPMS_MAIL_FROM_NAME_FORCE', true ); 
+    define( 'WPMS_MAILER', 'sendgrid' );
 
     define('WPMS_MAILER', 'sendgrid');
+    define( 'WPMS_SENDGRID_DOMAIN', '' );
     define( 'WPMS_SENDGRID_API_KEY', getenv('SENDGRID_API_KEY') );
-} */
+}
