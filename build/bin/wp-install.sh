27b078249b2ee0189238943350f8d235d5c012cd
admiralyeoj <joey.gross@1-commerce.com>
2025-01-10 16:06:24 -0500

    updates

--- build/bin/wp-install.sh
+++ build/bin/wp-install.sh
@@ -9,18 +9,6 @@ composer install > /var/www/html/composer.log 2>&1 || { echo "Composer install f
 
 echo "Website Composer install completed"
 
-
-cd web/app/themes/portfolio
-
-echo "Starting Composer install for theme"
-composer install > /var/www/html/composer.log 2>&1 || { echo "Composer install failed"; cat /var/www/html/composer.log; exit 1; }
-echo "Theme Composer install completed"
-
-echo "Starting Yarn install for theme"
-yarn install > /var/www/html/yarn.log 2>&1 || { echo "Yarn install failed"; cat /var/www/html/yarn.log; exit 1; }
-yarn build > /var/www/html/yarn.log 2>&1 || { echo "Yarn build failed"; cat /var/www/html/yarn.log; exit 1; }
-echo "Theme Yarn install completed"
-
 echo "Installing WordPress"
 wp core install --url=$WP_HOME \
   --title=wp \
