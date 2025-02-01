28cf2366486ddeaf01472b2d9761a89f05ae70c8
AdmiralYeoj <20649051+admiralyeoj@users.noreply.github.com>
2025-02-01 11:27:42 -0500

    allow indexing on production

--- wordpress/config/environments/production.php
+++ wordpress/config/environments/production.php
@@ -21,7 +21,7 @@ Config::define('WP_DEBUG_DISPLAY', false);
 Config::define('WP_DEBUG_LOG', env('WP_DEBUG_LOG') ?? false);
 Config::define('WP_DISABLE_FATAL_ERROR_HANDLER', false);
 Config::define('SCRIPT_DEBUG', false);
-Config::define('DISALLOW_INDEXING', true);
+Config::define('DISALLOW_INDEXING', false);
 
 ini_set('display_errors', '1');
 
