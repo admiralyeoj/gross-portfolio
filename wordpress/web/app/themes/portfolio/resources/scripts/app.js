3c21fa4555e278dab16d3ab4cb03849ee3b0281a
AdmiralYeoj <20649051+admiralyeoj@users.noreply.github.com>
2025-02-03 13:42:11 -0500

    updated brand icons and fixed js bug

--- wordpress/web/app/themes/portfolio/resources/scripts/app.js
+++ wordpress/web/app/themes/portfolio/resources/scripts/app.js
@@ -1,14 +1,15 @@
-import {domReady} from '@roots/sage/client';
-import 'bootstrap';
-import Isotope from 'isotope-layout';
+import {domReady} from "@roots/sage/client";
+import "bootstrap";
+import Isotope from "isotope-layout";
 
-$(window).on("load", function() {
-
+$(window).on("load", function () {
   isotopeMasonry();
-  
-  window.setInterval(function() {
-    animateCSS('.page-close', 'heartBeat');
-  }, 5000);
+
+  if (document.querySelector(".page-close")) {
+    window.setInterval(function () {
+      animateCSS(".page-close", "heartBeat");
+    }, 5000);
+  }
 
   $(".preloader").addClass("active");
   setTimeout(function () {
@@ -24,7 +25,7 @@ const main = async (err) => {
     // handle hmr errors
     console.error(err);
   }
-  
+
   animatedProgressBar();
   windowHieght();
 };
@@ -37,42 +38,43 @@ const main = async (err) => {
 domReady(main);
 import.meta.webpackHot?.accept(main);
 
-
-
 /**
  * Functions used
  */
- const animateCSS = (element, animation, prefix = 'animate__') =>
- // We create a Promise and return it
- new Promise((resolve) => {
-   const animationName = `${prefix}${animation}`;
-   const node = document.querySelector(element);
+const animateCSS = (element, animation, prefix = "animate__") =>
+  // We create a Promise and return it
+  new Promise((resolve) => {
+    const animationName = `${prefix}${animation}`;
+    const node = document.querySelector(element);
 
-   node.classList.add(`${prefix}animated`, animationName);
+    node.classList.add(`${prefix}animated`, animationName);
 
-   // When the animation ends, we clean the classes and resolve the Promise
-   function handleAnimationEnd(event) {
-     event.stopPropagation();
-     node.classList.remove(`${prefix}animated`, animationName);
-     resolve('Animation ended');
-   }
+    // When the animation ends, we clean the classes and resolve the Promise
+    function handleAnimationEnd(event) {
+      event.stopPropagation();
+      node.classList.remove(`${prefix}animated`, animationName);
+      resolve("Animation ended");
+    }
 
-   node.addEventListener('animationend', handleAnimationEnd, {once: true});
- });
+    node.addEventListener("animationend", handleAnimationEnd, {once: true});
+  });
 
-function animatedProgressBar () {
-  $(".progress").each(function() {
+function animatedProgressBar() {
+  $(".progress").each(function () {
     var skillValue = $(this).find(".skill-lavel").attr("data-skill-value");
-    $(this).find(".bar").animate({
-      width: skillValue,
-    }, 1500);
+    $(this).find(".bar").animate(
+      {
+        width: skillValue,
+      },
+      1500,
+    );
 
     $(this).find(".skill-lavel").text(skillValue);
   });
 }
 
-function windowHieght(){
-  if ( $(window).height() <=768 ) {
+function windowHieght() {
+  if ($(window).height() <= 768) {
     $(".pt-table").addClass("desktop-768");
   } else {
     $(".pt-table").removeClass("desktop-768");
@@ -83,19 +85,19 @@ function windowHieght(){
   Isotope Masonry
 ------------------------------------------*/
 function isotopeMasonry() {
-  var gutterSelector = document.querySelector('.isotope-gutter');
+  var gutterSelector = document.querySelector(".isotope-gutter");
   var iso;
-  if(gutterSelector) {
-    iso = new Isotope( gutterSelector, {
+  if (gutterSelector) {
+    iso = new Isotope(gutterSelector, {
       itemSelector: '[class^="col"]',
       percentPosition: true,
-      layoutMode: 'fitRows',
+      layoutMode: "fitRows",
     });
   }
 
-  var noGutterSelector = document.querySelector('.isotope-no-gutter');
-  if(noGutterSelector) {
-    new Isotope( noGutterSelector, {
+  var noGutterSelector = document.querySelector(".isotope-no-gutter");
+  if (noGutterSelector) {
+    new Isotope(noGutterSelector, {
       itemSelector: '[class^="col-"]',
       percentPosition: true,
       masonry: {
@@ -104,11 +106,11 @@ function isotopeMasonry() {
     });
   }
 
-  $(".filter a").on("click", function() {
+  $(".filter a").on("click", function () {
     $(".filter a").removeClass("active");
     $(this).addClass("active");
-    iso.arrange({ filter: $(this).attr("data-filter") });
-    
+    iso.arrange({filter: $(this).attr("data-filter")});
+
     return false;
   });
 }
